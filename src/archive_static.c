4f88abbce3fa9f6b9bfa30a8224b1ef7448a3270
Lena GIACALONE <lgiacalo@student.42.fr>
2019-03-22 21:12:00 +0100

    comm print

--- src/archive_static.c
+++ src/archive_static.c
@@ -26,13 +26,13 @@ void	ft_archive_static(void *ptr, int max)
 
 	if (!ft_record_symtab_header(&sym_h, ptr + 8) && !ft_is_safe(ptr + 68, sym_h.size))
 		return (ft_error_void3(env()->cmd, env()->file_name, ERROR3));
-	ft_fdprintf(FDD, "Max size archive : %d\n\n", max);
+	// ft_fdprintf(FDD, "Max size archive : %d\n\n", max);
 	ft_print_symtab_header(&sym_h);
 	tmp = ptr + 68 + sym_h.size;
 	while (tmp < (ptr + max))
 	{
 		ft_fdprintf(FDD, "\n\n------------------------------ BOUCLE ARCHIVE .O -------------------------\n");
-		ft_fdprintf(FDD, "Valeur tmp = %ld\n", (ptr + max - tmp));
+//		ft_fdprintf(FDD, "Valeur tmp = %ld\n", (ptr + max - tmp));
 		if (!ft_record_symtab_header(&sym_h, tmp))
 			return ; // TODO: ajouter erreur ou dans la fonction ft_record_symtab_header
 		ft_print_symtab_header(&sym_h);
