6b25d802db4fb81eb90169c1adb9b75307208325
Lena GIACALONE <lgiacalo@student.42.fr>
2019-04-04 16:55:28 +0200

    error otool on stderr

--- src/error.c
+++ src/error.c
@@ -14,27 +14,18 @@
 
 int		ft_error_int(char *str)
 {
-	if (!ft_strcmp(env()->cmd, OTOOL))
-		ft_fdprintf(1, "Error : %s\n", str);
-	else
-		ft_fdprintf(2, "Error : %s\n", str);
+	ft_fdprintf(2, "Error : %s\n", str);
 	return (EXIT_FAILUR);
 }
 
 int		ft_error_int3(char *str1, char *str2, char *str3)
 {
-	if (!ft_strcmp(env()->cmd, OTOOL))
-		ft_fdprintf(1, "%s%s%s\n", str1, str2, str3);
-	else
-		ft_fdprintf(2, "%s%s%s\n", str1, str2, str3);
+	ft_fdprintf(2, "%s%s%s\n", str1, str2, str3);
 	return (EXIT_FAILUR);
 }
 
 void	ft_error_void3(char *str1, char *str2, char *str3)
 {
-	if (!ft_strcmp(env()->cmd, OTOOL))
-		ft_fdprintf(1, "%s%s%s\n", str1, str2, str3);
-	else
-		ft_fdprintf(2, "%s%s%s\n", str1, str2, str3);
+	ft_fdprintf(2, "%s%s%s\n", str1, str2, str3);
 	return ;
 }
